43721c5adb577a78295a5ed4e16c9f67c18570ed
Beto Muniz <contato@betomuniz.com>
2014-03-26 23:36:45 -0300

    Commit init and replace text for *after* Dev in Company mode website.

---
+++ public/devincompany.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res){
+    fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+// app.get('/v2', function(req, res){
+//     fs.readFile(__dirname + '/index-v2.html', 'utf8', function(err, text){
+//         res.send(text);
+//     });
+// });
+
+app.listen(3001);
